31dfd7be9e0a03f3070797e99dde0f0e376ec8fd
cts <cts@ctsdns.com>
2016-01-04 19:36:41 +0800

    导航调整

--- Source/Blog/Blog.Web/App_Start/BundleConfig.cs
+++ Source/Blog/Blog.Web/App_Start/BundleConfig.cs
@@ -17,19 +17,20 @@ namespace Blog.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryAjax").Include(
                 "~/Scripts/jquery.unobtrusive-ajax.js"));
 
-            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
-            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css", "~/Content/site.css"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css"));
+
+
+            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
+            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
 
         }
 
