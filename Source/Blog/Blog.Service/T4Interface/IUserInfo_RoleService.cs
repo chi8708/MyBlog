758f13bb06ba03ef4b297be6679abd455e595e67
chi8708 <983143462@qq.com>
2016-11-03 17:34:04 +0800

    t4 模板生成错误重新安�

---
+++ Source/Blog/Blog.Service/T4Interface/IUserInfo_RoleService.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Entity.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Service
+{
+    public partial interface IUserInfo_RoleService:IBaseService<UserInfo_Role>
+    {
+    }
+}
