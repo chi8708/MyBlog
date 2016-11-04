f3814e0d9f442088608505d38072c6bb37a78367
chi8708 <983143462@qq.com>
2016-11-04 13:32:19 +0800

    他

---
+++ Source/Blog/Blog.Service/T4Interface/IArticle_CategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Entity.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Service
+{
+    public partial interface IArticle_CategoryService:IBaseService<Article_Category>
+    {
+    }
+}
